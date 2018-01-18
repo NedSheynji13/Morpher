65eaaac06f6e28bef485afe002cd84db7dcbbc04
NedSheynji13 <richardstrabl@gmail.com>
2018-01-18 23:38:54 +0100

    Added more scripts to outdated

---
+++ Assets/Scripts/Environment/Switches/Switch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+	#region Variables
+	#endregion
+	
+	void Start ()
+	{
+	}
+	
+	void Update () 
+	{
+	}
+}
