bb34f48ac68a8f7f6d5591cd3f956c4b7ededae1
NedSheynji13 <richardstrabl@gmail.com>
2018-01-19 02:34:03 +0100

    Switches have been implemented.

---
+++ Assets/Scripts/Environment/Switches/Affector.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Affector : MonoBehaviour
+{
+    #region Variables
+    public enum Options {MoveUp, MoveDown, MoveRight, MoveLeft};
+    public Options React;
+    public float destroyAfter;
+    [HideInInspector] public bool affected = false;
+	#endregion
+	
+	void Update () 
+	{
+        if (affected)
+        {
+            gameObject.GetComponent<Collider>().enabled = false;
+            switch (React)
+            {
+                case Options.MoveUp:
+                    {
+                        transform.Translate(transform.rotation * Vector3.up * 0.1f);
+                        break;
+                    }
+                case Options.MoveDown:
+                    {
+                        transform.Translate(transform.rotation * Vector3.down * 0.1f);
+                        break;
+                    }
+                case Options.MoveRight:
+                    {
+                        transform.Translate(transform.rotation * Vector3.back * 0.1f);
+                        break;
+                    }
+                case Options.MoveLeft:
+                    {
+                        transform.Translate(transform.rotation * Vector3.forward * 0.1f);
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+            StartCoroutine(Destroy(destroyAfter));
+        }
+	}
+
+    private IEnumerator Destroy(float _time)
+    {
+        yield return new WaitForSeconds(_time);
+        DestroyImmediate(gameObject);
+    }
+}
--- Assets/Scripts/Environment/Switches/Switch.cs
+++ Assets/Scripts/Environment/Switches/Switch.cs
@@ -4,14 +4,47 @@ using UnityEngine;
 
 public class Switch : MonoBehaviour
 {
-	#region Variables
-	#endregion
-	
-	void Start ()
-	{
-	}
-	
-	void Update () 
-	{
-	}
+    #region Variables
+    public bool isHeavy;
+    public Affector affector;
+
+
+    private bool pressed, hold;
+    private Vector3 maxHeight, minHeight;
+    #endregion
+
+    private void Start()
+    {
+        maxHeight = transform.position;
+        minHeight = maxHeight + (Vector3.down * 0.1f);
+        pressed = hold = false;
+    }
+    private void Update()
+    {
+        if (pressed)
+        {
+            transform.position = Vector3.Lerp(transform.position, minHeight, 5 * Time.deltaTime);
+            affector.affected = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isHeavy)
+        {
+            if (other.GetComponent<Rigidbody>() != null && other.GetComponent<Rigidbody>().mass > 5)
+                pressed = true;
+        }
+        else
+        {
+            if (other.GetComponent<Rigidbody>() != null)
+                pressed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Rigidbody>() != null)
+            pressed = false;
+    }
 }
